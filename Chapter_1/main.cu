5f620453c1b1350b873dd2c5ee43d9b9a891cc4f
Matthieu Zins <zins.matthieu@gmail.com>
2018-03-27 23:52:37 +0200

    c=a+b pass argument to kernel

--- Chapter_1/main.cu
+++ Chapter_1/main.cu
@@ -3,14 +3,27 @@
 
 using namespace std;
 
-__global__ void kernel(void) {
+__global__ void add(int a, int b, int *c) {
+    *c = a + b;
 
 }
 
 int main() {
+    int c(32);
+    int *dev_c;
+    cudaError_t alloc = cudaMalloc((void**)&dev_c, sizeof(int));
+    if (alloc != cudaSuccess) {
+        cerr << "Impossible to allocate memory: " << cudaGetErrorString(alloc) << endl;
+        exit(EXIT_FAILURE);
+    }
+    add<<<1, 1>>>(2, 7, dev_c);
 
-	kernel<<<1, 1>>>();
-	cout << "CUDA" << endl;
+    cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+    cout << "2 + 7 = " << c << endl;
 
-	return 0;
+    cudaFree(dev_c);
+
+    cout << "CUDA" << endl;
+
+    return 0;
 }
