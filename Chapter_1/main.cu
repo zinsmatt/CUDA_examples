afe860b3c7341bdde0321bfae3405109f6a216ff
Matthieu Zins <zins.matthieu@gmail.com>
2018-03-30 22:07:17 +0200

    add example for devices queries

--- Chapter_1/main.cu
+++ Chapter_1/main.cu
@@ -25,5 +25,29 @@ int main() {
 
     cout << "CUDA" << endl;
 
+    int count;
+    cudaGetDeviceCount(&count);
+    cout << "nb CUDA compatible devices: " << count << endl;
+
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+
+    cout << "Properties" << endl;
+    cout << "major: " << prop.major << endl;
+    cout << "minor: " << prop.minor << endl;
+    cout << "texture max dim: " << prop.maxTexture2D[0] << " " << prop.maxTexture2D[1] << endl;
+    cout << "nb multiprocessors: " << prop.multiProcessorCount << endl;
+    cout << "max threads per block " << prop.maxThreadsPerBlock << endl;
+    cout << "max thread dim: " << prop.maxThreadsDim[0] << " " << prop.maxThreadsDim[1] << "  " << prop.maxThreadsDim[2] << endl;
+    cout << "max grid dim: " << prop.maxGridSize[0] << " " << prop.maxGridSize[1] << "  " << prop.maxGridSize[2] << endl;
+
+    cout << "device name: " << prop.name << endl;
+    cout << "total global mem " << prop.totalGlobalMem << endl;
+    cout << "max shared mem per block: " << prop.sharedMemPerBlock << endl;
+    cout << "nb registers per block: " << prop.regsPerBlock << endl;
+    cout << "nb thread in a warp: " << prop.warpSize << endl;
+
+
+
     return 0;
 }
